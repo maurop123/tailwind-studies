4feaacc5cf19c19e8f6699e5b4a06e5f3808dbfb
Mauro Perez <maurop123@gmail.com>
2022-06-15 23:59:35 -0400

    [linked]

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
 <template>
     <div class="container mx-auto flex h-screen justify-center items-center">
-        <h1 class="font-sans text-6xl hover:text-sky-400/100 hover:underline">
-            <font-awesome-icon :icon="['fab', 'github']" />
-            maurop123
+        <h1>
+            <a href="https://github.com/maurop123"
+              class="font-sans text-6xl hover:text-sky-400/100 hover:underline"
+            >
+                <font-awesome-icon :icon="['fab', 'github']" />
+                maurop123
+            </a>
         </h1>
     </div>
 </template>
