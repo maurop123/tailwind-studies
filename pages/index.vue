92a933d2f3803256a2b0af640854082a0e22342d
Mauro Perez <maurop123@gmail.com>
2022-06-17 15:39:27 -0400

    add linked in. style for mobile. add profile pic

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,25 @@
 <template>
-    <div class="container mx-auto flex h-screen justify-center items-center">
-        <h1>
+    <div class="container mx-auto flex flex-wrap h-screen place-content-center grid grid-cols-1">
+        <div class="mx-auto">
+            <img src="~/assets/github-profile-pic.jpeg"
+                class="w-36 h-36 rounded-full drop-shadow-md mb-5"
+            />
+        </div>
+        <h1 class="mx-auto mb-7 text-5xl hover:text-sky-400/100">
+            <font-awesome-icon :icon="['fab', 'github']" />
             <a href="https://github.com/maurop123"
-              class="font-sans text-6xl hover:text-sky-400/100 hover:underline"
+              class="font-sans hover:italic underline hover:no-underline"
             >
-                <font-awesome-icon :icon="['fab', 'github']" />
                 maurop123
             </a>
         </h1>
+        <h2 class="mx-auto mb-5 text-4xl hover:text-sky-400/100">
+            <font-awesome-icon :icon="['fab', 'linkedin']" />
+            <a href="https://www.linkedin.com/in/mauro-perez-25634111/"
+              class="font-sans hover:italic underline hover:no-underline"
+            >
+                Mauro Perez
+            </a>
+        </h2>
     </div>
 </template>
--- plugins/fontAwesome.js
+++ plugins/fontAwesome.js
@@ -7,10 +7,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
 library.add(faGithub)
+library.add(faLinkedin)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
